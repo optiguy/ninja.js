0b6597250ed50a2c65d088256562a2c5488861d5
optiguy <bjbra@nuuday.dk>
2021-02-02 22:05:39 +0100

    Revert "refactor(App): remove old data source in html"
    This reverts commit 2ea12ef23674d6f64a9846bf05104e7efad7a089.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+var userData = JSON.parse(document.getElementById('user-data').dataset.users);
+ReactDOM.render(<App rows={userData} />, document.getElementById('root'));
